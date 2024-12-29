7603b2936098f9e36fa3d06cea51cc415e80634a
tmaker82 <tmaker82@gmail.com>
2024-12-29 22:25:00 +0300

    init

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload111.
+          Edit <code>src/App.js</code> and save to reload222.
         </p>
         <a
           className="App-link"
